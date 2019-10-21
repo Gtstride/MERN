4fa5a16f07fb9de6052da7b78b4a29986ea056e5
Godstime <lewisagho47@gmail.com>
2019-10-21 12:58:38 +0100

    Move server.js to backend folder

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => res.send('Hey! there, how dddo you do'));
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => )
