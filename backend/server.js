138f38cbc558fa8db5c619a955871aae5167ec0b
Godstime <lewisagho47@gmail.com>
2019-10-21 15:32:20 +0100

    Add COnfig Folder

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
-import express from 'express';
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hey! there, how dddo you do'));
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected Successfully'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hey! time to fly?'));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Web Running on port ${port}`));
+app.listen(port, () => console.log(`Web App is live on port ${port}`));
