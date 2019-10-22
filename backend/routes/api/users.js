69eb1f30f7e26e860fcdfb8661456f4fecabf16b
Godstime <lewisagho47@gmail.com>
2019-10-22 14:38:28 +0100

    Modif users.js file

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
+// Import User Model
+const User = require('../../models/User');
 
 /**
  * @route GET api/users/test
@@ -10,6 +14,47 @@ const router = express.Router();
 router.get('/test', (req, res) => 
   res.json({msg: "Users Works fine"}));
 
+  /**
+ * @route GET api/users/register
+ * @desc Allow Users to register
+ * @access Public
+ */ 
+
+ router.post('/register', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  })
+  .then(user => {
+    if(user) {
+      return res.status(400).json({ 
+        email: "Email already Exists"
+      });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //Size of gravatar
+        r: "pg", // Rating 
+        d: "mm" // Default
+      });
+      const newUser = new User({ 
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar 
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if(err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        })
+      })
+    }
+  })
+ });
 
 
 module.exports = router;
