1ae084ffa96b65d1595de4ca7e2010324d1b3a3c
Godstime <lewisagho47@gmail.com>
2019-10-22 18:25:20 +0100

    Add secret key

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI: 
-  "mongodb+srv://godstime:9OTpPdtiwoL7uEMQ@cluster0-rorws.mongodb.net/test?retryWrites=true&w=majority"
+  "mongodb+srv://godstime:9OTpPdtiwoL7uEMQ@cluster0-rorws.mongodb.net/test?retryWrites=true&w=majority",
+
+  secretorKey: 'godstime'
 };
