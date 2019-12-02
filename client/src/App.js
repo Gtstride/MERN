2f48b24b906535558babd5ad0fcbf93deb3f1091
Godstime <lewisagho47@gmail.com>
2019-12-02 12:59:27 +0100

    Complete login, register, redirection, Authentication, and Landing front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,59 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import store from "./store";
+
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
 import Landing from "./components/layouts/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 import "./App.css";
+
+// Check for Token
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken); // Set auth token header auth
+
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser()); // Logout User
+
+    // TODO: clear current profile
+    
+
+    // Redirect to login
+    window.location.href = '/login'
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
