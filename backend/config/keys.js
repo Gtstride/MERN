5886881d451f51baee2fed1f53a641adde39c881
Godstime <lewisagho47@gmail.com>
2019-12-18 13:44:18 +0100

    Configured my key's folder for bothe production and development

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,7 +1,7 @@
-module.exports = {
-  mongoURI:
-  // 'mongodb+srv://godstime:eWC2hZqacjFP8KZH@cluster0-7achi.mongodb.net/test?retryWrites=true&w=majority',
-  'mongodb+srv://godstime:9OTpPdtiwoL7uEMQ@cluster0-rorws.mongodb.net/test?retryWrites=true&w=majority',
+/* eslint-disable global-require */
 
-  secretOrKey: 'godstime',
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
